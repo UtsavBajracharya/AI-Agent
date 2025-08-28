2e3b5273c1485d5313becf071043b3f01c5c487f
Utsav <utsavbajra619@gmail.com>
2025-08-28 16:28:41 -0400

    basic code setups and imports

---
+++ main.py
@@ -0,0 +1,17 @@
+# Setup simple agent
+
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from langchain_openai import ChatOpenAI
+from langchain_authropic import ChatAnthropic
+
+load_dotenv() # load the environment variable we created in env 
+
+# Setup in LLM, give it to an agent and then give the agent some tools
+
+# llm = ChatOpenAI(model= "gpt-4o-mini") # use can use openai key or anything
+llm = ChatAnthropic(model = "claude-3-5-sonnet-20241022") # add the model with version
+
+# Test your API key
+# response = llm.invoke("What is the meaning of life?")
+# print(response)
