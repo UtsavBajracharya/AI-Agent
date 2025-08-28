237edf6bcb3265ebc65fffabe34ae76598b544d6
Utsav <utsavbajra619@gmail.com>
2025-08-28 16:41:09 -0400

    using claude or gpt api key

--- main.py
+++ main.py
@@ -3,15 +3,15 @@
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
-from langchain_authropic import ChatAnthropic
+from langchain_anthropic import ChatAnthropic
 
 load_dotenv() # load the environment variable we created in env 
 
 # Setup in LLM, give it to an agent and then give the agent some tools
 
-# llm = ChatOpenAI(model= "gpt-4o-mini") # use can use openai key or anything
-llm = ChatAnthropic(model = "claude-3-5-sonnet-20241022") # add the model with version
+llm = ChatOpenAI(model= "gpt-4o-mini") # use can use openai key or anything
+# llm = ChatAnthropic(model = "claude-3-5-sonnet-20241022") # add the model with version
 
 # Test your API key
-# response = llm.invoke("What is the meaning of life?")
-# print(response)
+response = llm.invoke("What is the meaning of life?")
+print(response)
